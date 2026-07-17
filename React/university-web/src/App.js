03671ac0261322bc0832c5133f59b1e0d0d52496
User <user@example.com>
2026-07-17 09:48:37 +0530

    h

--- React/university-web/src/App.js
+++ React/university-web/src/App.js
@@ -27,6 +27,27 @@ function App() {
         studentId="s003"
         major="Culinary Arts"
       />
+
+      <h2>Available Courses</h2>
+      
+      {/* Reusing the same component with different data */}
+      <CourseCard
+        title="Introduction to Programming"
+        code="CS101"
+        credits={3}
+      />
+
+      <CourseCard
+        title="Data Structures"
+        code="CS201"
+        credits={4}
+      />
+
+      <CourseCard
+        title="Operating Systems"
+        code="CS301"
+        credits={4}
+      /> 
     </div>
   );
 }
