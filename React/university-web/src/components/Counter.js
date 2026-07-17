258c5a2e24aece16c08aeb690e4bd97c7e6d2c5c
User <user@example.com>
2026-07-17 10:18:05 +0530

    d

--- React/university-web/src/components/Counter.js
+++ React/university-web/src/components/Counter.js
@@ -8,3 +8,11 @@ function Counter() {
 
   const decrement = () => {
     if (const count > 0) {
+        setCount(count - 1);
+    }
+};
+
+return {
+    <div style = {{ textAlign: 'center' }}>
+        <h1>Class Attendance Counter</h1>
+}
