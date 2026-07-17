244db62b212a7e606b4b2e2531cee5b07b160578
User <user@example.com>
2026-07-17 10:20:37 +0530

    e

--- React/university-web/src/components/Counter.js
+++ React/university-web/src/components/Counter.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
+
 function Counter() {
+  // 1. Initialize state
+  // count starts at 0, setCount is the tool to change it.
   const [count, setCount] = useState(0);
 
+  // 2. Event Handlers
   const increment = () => {
+    // We do NOT say: count = count + 1 (this is forbidden)
+    // We MUST use the setter:
     setCount(count + 1);
-  }
+  };
 
   const decrement = () => {
-    if (const count > 0) {
-        setCount(count - 1);
+    if (count > 0) {
+      setCount(count - 1);
     }
-};
+  };
 
-return {
-    <div style = {{ textAlign: 'center' }}>
-        <h1>Class Attendance Counter</h1>
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h3>Class Attendance Counter</h3>
+      <h1>{count}</h1>
+      <button onClick={increment}>Add Student</button>
+      <button onClick={decrement}>Remove Student</button>
+    </div>
+  );
 }
+
+export default Counter;
