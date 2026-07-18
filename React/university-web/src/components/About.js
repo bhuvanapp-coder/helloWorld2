65115ee4b4dd220ae5100e3bdfea351fc8ef1901
User <user@example.com>
2026-07-18 08:33:38 +0530

    y

---
+++ React/university-web/src/components/About.js
@@ -0,0 +1,4 @@
+function About() {
+    return <h2>About University Hub</h2>;
+}
+export default About;
---
+++ React/university-web/src/components/Home.js
@@ -0,0 +1,9 @@
+function Home(){
+    return (
+        <div>
+            <h1>Welcome to the University Website</h1>
+            <p>Manage students, courses, and academic activities</p>
+        </div>
+    );
+}
+export default Home;
